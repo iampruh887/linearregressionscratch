69d01ac84d2f57f864791e8ee80226a8991e9147
iampruh887 <iampruh887@gmail.com>
2024-05-16 23:30:14 +0530

    implemented linear regression on the file testing.csv (src: kaggle) achieved: an accuracy of 65.5% with a split of 70-30 an accuracy of 66.66% with a split of 60-40 => diving into 60-40 split, 66.66% was achieved by a tolerance of .50 66.15% was achieved by a tolerance of .60 69.69% was achieved by a tolerance of .40 ~47.37% was achieved by a tolerance of 0.30
    therefore as determined for the given dataset i.e. testing.csv,
    a split of 60-40 and a threshold of 0.40 yields the maximum accuracy for the model
    an accuracy of 63.3% with a split of 50-50

--- linearregression.py
+++ linearregression.py
@@ -1,11 +1,12 @@
 import numpy as np
+import math
 def findindex(List, val) -> int:
 	ctr = 0
-	val = val.replace(" ", "")
 	for i in List:
-		if i.lower() == val.lower():
+		if i == val:
 			return ctr
 		ctr+=1
+
 def readfile(filename) -> tuple:
 
 	"""
@@ -52,15 +53,28 @@ def readfile(filename) -> tuple:
 		strn = ""
 
 	titles = OrigList[0]	
-	print(titles)
 	ct = 0
+	invalid = []
 	del OrigList[0]
 	tempOrigList = []
-
+	for i in OrigList:
+		for j in i:
+			if j == "":
+				invalid.append(ct)
+		ct+=1
+	t = []
+	i = 0
+	while i<len(OrigList):
+		if i not in invalid:
+			t.append(OrigList[i])
+		i+=1
+	OrigList = t
+	del t
+	
 	for i in OrigList:
 		temp = []
 		for j in i:
-			temp.append(np.int64(j))
+			temp.append(np.float64(j))
 		tempOrigList.append(temp)
 	OrigList = np.array(tempOrigList)
 	# obtained all values in a vector format		
@@ -68,4 +82,106 @@ def readfile(filename) -> tuple:
 output = readfile("tested.csv")
 values= output[0]
 titles = output[1]
+print(titles)
+k = len(values)
+num = math.ceil(k*0.7)
+i = 0
+training = []
+testing = []
+while i<num:
+	training.append(values[i])
+	i+=1
+i = num 
+while i<k:
+	testing.append(values[i])
+	i+=1
 
+X = np.array([])
+Klist = training
+ct = 0
+X = np.array([])
+for i in Klist:
+		temp = np.array([np.float64(i[2]), np.float64(i[3]), np.float64(i[6])])
+		if X.size == 0:
+			X = temp
+		else:
+			X = np.vstack((X, temp))
+y_hat = []
+y = []
+for i in training:
+	y.append(np.float64(i[1]))
+m = len(X)
+W = np.array([])
+ds = np.float64(0)
+for i in range(3): 
+	W = np.append(W, np.array([ds]))
+alpha = 0.0001
+epochs = 100000
+#gradient descent
+prev_cost = float('inf') 
+tolerance = 0.000001
+for i in range(epochs):
+    f = np.dot(X, W)
+    error = f - y 
+    gradients = (1/float(m)) * np.dot(X.T, error)
+    W = W - alpha * gradients
+    J = (1/float(2*m)) * np.sum(error**2)
+    #print(f"Iteration {i+1}, cost: {J}")
+
+    # Check convergence by comparing current cost with previous cost
+    if abs(J - prev_cost) < tolerance:
+     #   print("Converged!")
+        break
+
+    prev_cost = J
+    
+
+
+"""
+print(W)
+print(titles)
+pclass = np.float64(input("enter Pclass: "))
+age = np.float64(input("enter age"))
+fare = np.float64(input("enter fare"))
+X_hat = np.array([pclass, age, fare])
+f = np.dot(X_hat, W)
+print(f)
+"""
+X = np.array([])
+Klist = testing
+for i in Klist:
+		temp = np.array([np.float64(i[2]), np.float64(i[3]), np.float64(i[6])])
+		if X.size == 0:
+			X = temp
+		else:
+			X = np.vstack((X, temp))
+
+y = []
+for i in Klist:
+	y.append(np.float64(i[1]))
+
+n = len(X)
+acc = []
+for i in range(n):
+	f = np.dot(X, W)
+	#print(f)
+	fin = float(f[i])*100
+	
+	g = None
+	lmn = float(50)
+	print(fin, lmn)
+	if fin>lmn:
+		g = 1
+	else:
+		g = 0
+	if g == y[i]:
+		acc.append(1)
+	else:
+		acc.append(0)
+correct = 0
+for i in acc:
+	if i == 1:
+		correct += 1
+accuracy = (correct/float(len(acc)))*100
+print(f"accuracy = {accuracy}")
+#print(X, y)
---
+++ test.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr = np.array([[1,2,3],[[2,3,4],[4,1,3]]])
+print(np.shape(arr))
